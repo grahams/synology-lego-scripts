2bd778e7c9f3d94f5f7c0c33f7cb9cfc02fd0968
Sean Graham <github@sean-graham.com>
2020-12-01 16:39:23 -0500

    removed hardcoded dirs

--- initialRequestCerts.sh
+++ initialRequestCerts.sh
@@ -6,7 +6,7 @@ source "${DIR}/config.sh"
 LEGO_FOLDER="${DIR}/.lego/certificates"
 SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
 
-docker run --rm -e LINODE_TOKEN_FILE=/ssl/linode-key -e LINODE_POLLING_INTERVAL=60 -e LINODE_PROPAGATION_TIMEOUT=0 -v ~/lego-ssl:/ssl goacme/lego -a -k rsa4096 -d ${LE_DOMAIN} -m ${LE_EMAIL_ADDRESS} --dns=linodev4 --path=/ssl/.lego run
+docker run --rm -e LINODE_TOKEN_FILE=/ssl/linode-key -e LINODE_POLLING_INTERVAL=60 -e LINODE_PROPAGATION_TIMEOUT=0 -v ${DIR}:/ssl goacme/lego -a -k rsa4096 -d ${LE_DOMAIN} -m ${LE_EMAIL_ADDRESS} --dns=linodev4 --path=/ssl/.lego run
 
 echo "now manually install the certificates in the Synology GUI, then discover the Cert ID from /usr/syno/etc/certificate/_archive/INFO, and insert it into config.sh"
 
--- renewCerts.sh
+++ renewCerts.sh
@@ -6,7 +6,7 @@ source "${DIR}/config.sh"
 LEGO_FOLDER="${DIR}/.lego/certificates"
 SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
 
-docker run --rm -e LINODE_TOKEN_FILE=/ssl/linode-key -e LINODE_POLLING_INTERVAL=60 -e LINODE_PROPAGATION_TIMEOUT=0 -v ~/lego-ssl:/ssl goacme/lego -a -k rsa4096 -d ${LE_DOMAIN} -m ${LE_EMAIL_ADDRESS} --dns=linodev4 --path=/ssl/.lego renew --days 20 | grep -i "no renewal" > /dev/null
+docker run --rm -e LINODE_TOKEN_FILE=/ssl/linode-key -e LINODE_POLLING_INTERVAL=60 -e LINODE_PROPAGATION_TIMEOUT=0 -v ${DIR}:/ssl goacme/lego -a -k rsa4096 -d ${LE_DOMAIN} -m ${LE_EMAIL_ADDRESS} --dns=linodev4 --path=/ssl/.lego renew --days 20 | grep -i "no renewal" > /dev/null
 
 code=$?
 if [ $code -ne 0 ]; then
