7ea0d99740513f0bac75d2867f39d6354d73c493
Sean Graham <github@sean-graham.com>
2021-04-26 09:16:17 -0400

    tweaked paths

--- installCerts.sh
+++ installCerts.sh
@@ -2,7 +2,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/config.sh"
 
 LEGO_FOLDER="${DIR}/.lego/certificates"
-SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
+#SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
+SYNOLOGY_FOLDER="/usr/syno/etc/certificate/system/default/"
+
+cp "${LEGO_FOLDER}"/"${LE_DOMAIN}".crt "${SYNOLOGY_FOLDER}"/cert.pem
+cp "${LEGO_FOLDER}"/"${LE_DOMAIN}".issuer.crt "${SYNOLOGY_FOLDER}"/chain.pem
+cat "${LEGO_FOLDER}"/"${LE_DOMAIN}".crt "${LEGO_FOLDER}"/"${LE_DOMAIN}".issuer.crt > "${SYNOLOGY_FOLDER}"/fullchain.pem
+cp "${LEGO_FOLDER}"/"${LE_DOMAIN}".key "${SYNOLOGY_FOLDER}"/privkey.pem
+
+SYNOLOGY_FOLDER="/usr/syno/etc/certificate/system/FQDN/"
 
 cp "${LEGO_FOLDER}"/"${LE_DOMAIN}".crt "${SYNOLOGY_FOLDER}"/cert.pem
 cp "${LEGO_FOLDER}"/"${LE_DOMAIN}".issuer.crt "${SYNOLOGY_FOLDER}"/chain.pem
--- renewCerts.sh
+++ renewCerts.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/config.sh"
 
 LEGO_FOLDER="${DIR}/.lego/certificates"
-SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
+#SYNOLOGY_FOLDER="/usr/syno/etc/certificate/_archive/${SYNOLOGY_CERT_ID}"
+SYNOLOGY_FOLDER="/usr/syno/etc/certificate/system/default/"
 
 docker run --rm -e LINODE_TOKEN_FILE=/ssl/linode-key -e LINODE_POLLING_INTERVAL=60 -e LINODE_PROPAGATION_TIMEOUT=0 -v "${DIR}":/ssl goacme/lego -a -k rsa4096 -d ${LE_DOMAIN} -m ${LE_EMAIL_ADDRESS} --dns=linodev4 --path=/ssl/.lego renew --days 20 | grep -i "no renewal" > /dev/null
 
